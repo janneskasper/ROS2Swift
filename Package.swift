2d6d465758e8c38d294e084551ba1014fd6ad0e9
Jannes Kasper <jannes.kasper@gmail.com>
2023-07-26 01:12:15 +0200

    updated package

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/janneskasper/FastRTPSSwift.git", branch: "bugfix"),
-        .package(path: "/Users/jannes.kasper/Documents/git/FastRTPSSwift")
+        .package(url: "https://github.com/janneskasper/FastRTPSSwift.git", branch: "bugfix"),
+//        .package(path: "/Users/jannes.kasper/Documents/git/FastRTPSSwift")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
